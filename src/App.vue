98252a32184564247cc6132d5c3dbcff7d82ee05
parrotdance <parrotdance@foxmail.com>
2017-10-27 17:50:30 +0800

    10.27 - Begin

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <mycheckin></mycheckin>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import mycheckin from './components/myCheckIn';
+
+export default {
+  name: 'app',
+  components: {
+    mycheckin
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 0;
+  margin: 0;
+  color: white;
+}
+</style>
---
+++ src/components/myCheckIn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mycheckin">
+    <div class="wrapper">
+      <div class="myName">myName</div>
+      <div class="myAims">myAims</div>
+      <div class="keepDays">keepDays</div>
+      <div class="lastcheck">lastcheck</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style scoped>
+  .mycheckin {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(90, 183, 131);
+    font-size: 2rem;
+  }
+  .wrapper {
+    padding: 1.5rem;
+  }
+</style>
